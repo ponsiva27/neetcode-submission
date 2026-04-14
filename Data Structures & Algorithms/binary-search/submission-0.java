074e45a8c575e39359f007e9ae4f4480a2816ce6
Ponsiva J <jkponsiva@gmail.com>
2026-04-14 18:11:27 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        if(nums==null) {
+            return 0;
+        }
+
+        int left=0;
+        int right=nums.length-1;
+
+        while(left<=right) {  //to seach as long as search space exhaust
+            
+            int mid = left+(right-left)/2;
+
+            if(nums[mid]==target) {
+                return mid;
+            } else if (nums[mid]>target) {
+                right = mid-1;
+            } else {
+                left = mid+1;
+            }
+        }
+        return -1; //if target doesn't exist in the given array
+    }
+}
