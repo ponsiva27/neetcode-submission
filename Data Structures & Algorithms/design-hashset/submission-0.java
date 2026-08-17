294659d6e9f175ccf5d9dbb2316a607e8de71b7c
Ponsiva J <jkponsiva@gmail.com>
2026-08-17 08:08:32 +0530

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.java
@@ -0,0 +1,55 @@
+class MyHashSet {
+
+    private int ARRAY_SIZE = 10000;
+
+    private LinkedList<Integer>[] buckets;
+
+    public MyHashSet() {
+        buckets = new LinkedList[ARRAY_SIZE];
+    }
+
+    private int hash(int key) {
+        return key%ARRAY_SIZE;
+    }
+    
+    public void add(int key) {
+        int index = hash(key);
+
+        // If there's no bucket here yet, build one
+        if (buckets[index] == null) {
+            buckets[index] = new LinkedList<>();
+        }
+        
+        // Only add if it's not already in the bucket (HashSets hold UNIQUE values)
+        if (!buckets[index].contains(key)) {
+            buckets[index].add(key);
+        }
+    }
+    
+    public void remove(int key) {
+        int index = hash(key);
+
+        LinkedList<Integer> bucket = buckets[index];
+        if(bucket !=null) {
+            bucket.remove(Integer.valueOf(key));
+        }   
+    }
+    
+    public boolean contains(int key) {
+        int index = hash(key);
+        LinkedList<Integer> bucket = buckets[index];
+
+        if(bucket!=null) {
+            return bucket.contains(key);
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
