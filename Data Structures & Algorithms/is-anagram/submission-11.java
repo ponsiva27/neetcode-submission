e0cae7ee38a916c023f3ed94081df6754dac9ceb
Ponsiva J <jkponsiva@gmail.com>
2026-09-14 07:40:10 +0530

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s== null || t==null|| s.length()!=t.length()) {
+            return false;
+        }
+
+        int[] arr = new int[26];
+
+        for(int i=0;i<s.length();i++) {
+             arr[s.charAt(i)-'a']++;
+
+             arr[t.charAt(i)-'a']--;
+        }
+
+        for(int num : arr) {
+            if(num!=0) {
+                return false;
+            }
+         }
+        return true;
+    }
+}
