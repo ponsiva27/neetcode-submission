313eaf4b18373eabc15ae76dfde35c2a43870902
Ponsiva J <jkponsiva@gmail.com>
2026-09-14 10:29:40 +0530

    Add: remove-element - submission-3

---
+++ Data Structures & Algorithms/remove-element/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+        int read =0;
+        int write =0;
+
+        while(read < nums.length) {
+            if(nums[read]!=val){
+                nums[write] =nums[read];
+                write++;
+            }
+            read++;
+        }
+        return write;
+    }
+}
