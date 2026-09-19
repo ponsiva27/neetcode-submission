f79f2fd7dcd34d19de0d6859fe45c84aeff9fe59
Ponsiva J <jkponsiva@gmail.com>
2026-09-19 19:08:45 +0530

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        if(nums==null || nums.length==0) {
+            return 0;
+        }
+
+        int candidate = nums[0];
+        int count=0;
+        for(int i=0;i<nums.length;i++) {
+
+            if(count==0) {
+                candidate= nums[i];
+            }
+            if(nums[i]==candidate) {
+                 count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
